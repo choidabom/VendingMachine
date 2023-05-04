801b0033e12d12723c654aac9d1869cb8cea2349
choidabom <devom@pioncorp.com>
2023-05-04 12:54:21 +0900

    chore: move to client side

--- server/input/readlineInput.ts
+++
@@ -1,8 +0,0 @@
-import * as readline from "readline";
-import process from "process";
-
-// create interface for input and output
-export const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
