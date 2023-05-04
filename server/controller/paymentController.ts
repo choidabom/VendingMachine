7b7dcb9abc0d52837bb319a8c8b03dbe081d95b8
choidabom <devom@pioncorp.com>
2023-05-04 12:53:33 +0900

    feat: divide selling logic to paymentController, saleController

--- server/controller/paymentController.ts
+++ server/controller/paymentController.ts
@@ -1,88 +1,82 @@
-// import { ProductEntity } from '../entity/ProductEntity';
-// import { checkLeftChange } from "./../models/models";
-// import { db, transaction } from "../db/db";
+import { checkLeftChange } from "./../models/ResourceModel";
+import { leftVMMoneyAfterPayment } from '../models/OrderModel';
 
-// interface IPaymentService {
-//     pay(vmID: number, priceSum: number, inputMoney: number): Promise<boolean>;
-// }
+interface IPaymentService {
+    pay(vmID: number, priceSum: number, inputMoney: number): Promise<boolean>;
+}
 
-// class CashPaymentService implements IPaymentService {
-//     pay(vmID: number, priceSum: number, inputMoney: number): Promise<boolean> {
-//         return new Promise<boolean>(async (resolve, reject) => {
-//             const price = inputMoney;
-//             let leftMoney = await checkLeftChange(vmID);
+class CashPaymentService implements IPaymentService {
+    pay(vmID: number, priceSum: number, inputMoney: number): Promise<boolean> {
+        return new Promise<boolean>(async (resolve, reject) => {
+            const price: number = inputMoney;
+            let leftVMMoney: number = await checkLeftChange(vmID); // 자판기 잔액
 
-//             if (isNaN(price) || price < priceSum) {
-//                 console.log("지불 금액이 부족하거나 올바르지 않습니다. 다시 입력해주세요.");
-//                 resolve(false);
-//             }
-//             else {
-//                 const change = price - priceSum;  // 반환 금액 = 입금액 - 상품 총합
+            if (isNaN(price) || price < priceSum) {
+                console.log("지불 금액이 부족하거나 올바르지 않습니다. 다시 입력해주세요.");
+                resolve(false);
+            }
+            else {
+                const returnChange = price - priceSum;  // 반환 금액 = 입금액 - 상품 총합
 
-//                 // 정상적 현금 결제
-//                 if (leftMoney - change >= 0 && change >= 0) {
-//                     console.log(`현금 결제 : ${priceSum}원입니다. `);
-//                     console.log(`반환 금액 : ${change}원입니다. `);
+                // 정상적 현금 결제
+                if (leftVMMoney - returnChange >= 0 && returnChange >= 0) {
+                    console.log(`현금 결제 : ${priceSum}원입니다. `);
+                    console.log(`반환 금액 : ${returnChange}원입니다. `);
+                    if (await leftVMMoneyAfterPayment(vmID, leftVMMoney, returnChange)) {
+                        resolve(true);
+                    } else {
+                        resolve(false);
+                    }
+                }
+                // 비정상적 현금 결제 
+                else {
+                    console.log(`기기 내 잔액 부족으로 반환 불가로 이용할 수 없습니다.`);
+                    console.log(`[판매] - 기기 내 잔액 : ${leftVMMoney}`);
+                    resolve(false);
+                }
+            }
+        });
+    }
+}
 
-//                     // 결제 잔액 DB(vm_resource 테이블)에 저장 
-//                     const leftChange = leftMoney - change;
-//                     const leftChangeSQL = `
-//                             UPDATE vm_resource
-//                             SET quantity = ${leftChange}
-//                             WHERE vm_id = ${vmID} AND name = 'cash'
-//                     `;
-//                     await transaction(leftChangeSQL);
-//                     resolve(true);
-//                 }
-//                 // 비정상적 현금 결제 
-//                 else {
-//                     console.log(`기기 내 잔액 부족으로 반환 불가로 이용할 수 없습니다.`);
-//                     console.log(`[판매] - 기기 내 잔액 : ${leftMoney}`);
-//                     resolve(false);
-//                 }
-//             }
-//         });
-//     }
-// }
+class CardPaymentService implements IPaymentService {
+    pay(vmID: number, priceSum: number, inputMoney: number): Promise<boolean> {
 
-// class CardPaymentService implements IPaymentService {
-//     pay(vmID: number, priceSum: number, inputMoney: number): Promise<boolean> {
-//         // 카드 결제는 일단 always true로 설정
-//         const isAvailable = true;
+        return new Promise<boolean>((resolve, reject) => {
+            let leftCardMoney = inputMoney - priceSum;
+            console.log(`카드 결제 : ${priceSum}원입니다. `);
+            console.log('결제 중 입니다...');
 
-//         return new Promise((resolve, reject) => {
-//             console.log(`카드 결제 : ${priceSum}원입니다. `);
-//             console.log('결제 중 입니다...');
-//             if (isAvailable) { // 카드 결제 성공
-//                 setTimeout(() => {
-//                     console.log("[판매] 카드 결제를 완료하였습니다. ");
-//                     resolve(true);
-//                 }, 2000);
-//             } else { // 카드 결제 실패
-//                 setTimeout(() => {
-//                     console.log("[판매] 카드 결제를 실패하였습니다. ");
-//                     resolve(false);
-//                 }, 2000);
-//             }
-//         });
-//     }
-// }
+            if (leftCardMoney >= 0) { // 카드 결제 성공
+                setTimeout(() => {
+                    console.log("[판매] 카드 결제를 완료하였습니다. ");
+                    resolve(true);
+                }, 2000);
+            } else { // 카드 결제 실패
+                setTimeout(() => {
+                    console.log("[판매] 카드 결제를 실패하였습니다. ");
+                    resolve(false);
+                }, 2000);
+            }
+        });
+    }
+}
 
-// class CoinPaymentService implements IPaymentService {
-//     pay(vmID: number, priceSum: number): Promise<boolean> {
-//         throw new Error('Method not implemented.');
-//     }
-// }
+class CoinPaymentService implements IPaymentService {
+    pay(vmID: number, priceSum: number): Promise<boolean> {
+        throw new Error('Method not implemented.');
+    }
+}
 
-// export type PAYMENT_TYPE = "card" | "cash" | "coin";
+export type PAYMENT_TYPE = "card" | "cash" | "coin";
 
-// export class PaymentController {
-//     supportPayments = {
-//         "card": new CardPaymentService(),
-//         "cash": new CashPaymentService(),
-//         "coin": new CoinPaymentService(),
-//     };
-//     pay(paymentType: PAYMENT_TYPE, vmID: number, priceSum: number, inputMoney: number): Promise<boolean> {
-//         return this.supportPayments[paymentType].pay(vmID, priceSum, inputMoney);
-//     }
-// }
+export class PaymentController {
+    supportPayments = {
+        "card": new CardPaymentService(),
+        "cash": new CashPaymentService(),
+        "coin": new CoinPaymentService(),
+    };
+    pay(paymentType: PAYMENT_TYPE, vmID: number, priceSum: number, inputMoney: number): Promise<boolean> {
+        return this.supportPayments[paymentType].pay(vmID, priceSum, inputMoney);
+    }
+}
---
+++ server/controller/saleController.ts
@@ -0,0 +1,31 @@
+import { PaymentController, PAYMENT_TYPE } from "./paymentController";
+
+// 제품 판매 함수
+async function payProductPrice(vmID: number, priceSum: number, paymentMethod: number, inputMoney: number): Promise<boolean> {
+    let isOkay: boolean = true;
+    const paymentController = new PaymentController();
+    if (isNaN(paymentMethod) || paymentMethod < 1 || paymentMethod > 2) {
+        console.log("지불 방법이 올바르지 않습니다.");
+        isOkay = false;
+    } else {
+        let paymentType: PAYMENT_TYPE = undefined!;
+        switch (paymentMethod) {
+            case 1:
+                paymentType = "cash";
+                break;
+            case 2:
+                paymentType = "card";
+                break;
+            default:
+                throw new Error('unsupported payment type');
+        }
+        if (!await paymentController.pay(paymentType, vmID, priceSum, inputMoney)) {
+            console.log("결제에 실패했습니다.");
+            isOkay = false;
+        }
+    }
+    return isOkay;
+};
+
+export { payProductPrice };
+
