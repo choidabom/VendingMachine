a433f74555f760e008ca17e53d51958a5a595628
choidabom <devom@pioncorp.com>
2023-06-20 15:51:42 +0900

    fix: SERVER ROUTES - Spring boot 8080

---
+++ spring-client/src/Config.tsx
@@ -0,0 +1,3 @@
+// SERVER ROUTES
+// Node.js(express)의 포트는 3000, Spring boot의 포트는 8080
+export const API_URL = "http://localhost:8080/vending_machine";
--- spring-client/src/components/Config.tsx
+++
@@ -1,2 +0,0 @@
-// SERVER ROUTES
-export const API_URL = "http://localhost:5173/vending_machine";
