9430cb1a3245fc0acef470e981989c985b29bc67
choidabom <devom@pioncorp.com>
2023-06-23 17:49:32 +0900

    cli: rename DTo -> DTO

--- vending-machine-spring/src/main/java/com/spring/vendingmachinespring/dto/ProductResourceDTo.java
+++ vending-machine-spring/src/main/java/com/spring/vendingmachinespring/dto/ProductResourceDTo.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class ProductResourceDTo {
+public class ProductResourceDTO {
     private Long id;
     private Long productId;
     private Long resourceId;
